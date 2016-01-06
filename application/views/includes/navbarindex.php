9d2960a0938d28e1b29c38694e3ee8dd838e6591
patrikejane <sskmal@gmail.com>
2016-01-06 17:34:21 +0530

    new index

---
+++ application/views/includes/navbarindex.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Smalkakulage
+ * Date: 8/28/15
+ * Time: 1:08 PM
+ */
+?>
+
+
+<div class="navbar navbar-blue  navbar-inverse navbar-static-top">
+    <div class="container">
+    <div class="navbar-header">
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand">VideoBay</a>
+    </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#" >About</a></li>
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#">Sign up</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign in</a>
+                        <div class="dropdown-menu" style="padding: 15px; padding-bottom:  0px;">
+                            <form method="post" action="login" accept-charset="UTF-8">
+                                <input style="margin-bottom: 15px;" type="text" placeholder="Username" id="username" name="username">
+                                <input style="margin-bottom: 15px;" type="password" placeholder="password" id="password" name="password">
+                                <input style="float: left; margin-right: 10px;" type="checkbox" name="remember-me" id="remember-me" value="1">
+                                <label class="string optional" for="user_remember_me">Remember me</label>
+                                <input class="btn btn btn-success" type="submit" id="sign-in" value="Sign In">
+                            </form>
+
+                        </div>
+
+                    </li>
+
+                </ul>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+
