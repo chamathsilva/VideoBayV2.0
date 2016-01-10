39c15b68f49c15bdbe2cafff1b60648108812527
Chamath Silva <mbckchamathsilva@gmai.com>
2016-01-11 03:06:23 +0530

    fix error class

--- application/views/admin/addLessons.php
+++ application/views/admin/addLessons.php
@@ -171,12 +171,12 @@
                 //extension: "txt",
             },
             "subject[]":{
-                //required: true,
-                //minlength:1,
+                required: true,
+                minlength:1,
             },
             "users[]":{
-                //required: true,
-                //minlength:1,
+                required: true,
+                minlength:1,
             }
 
         },
@@ -197,6 +197,26 @@
             }
 
         },
+
+        errorClass: 'help-block',
+        highlight: function(element){
+            $(element)
+                .closest('.form-group')
+                .addClass('has-error');
+        },
+
+        unhighlight: function(element){
+            $(element)
+                .closest('.form-group')
+                .removeClass('has-error');
+        },
+        errorPlacement: function(error,element){
+            if (element.prop('type') === 'checkbox'){
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        }
 
     });
 
--- application/views/user/registration.php
+++ application/views/user/registration.php
@@ -169,6 +169,25 @@
             }
 
 
+        },
+        errorClass: 'help-block',
+        highlight: function(element){
+            $(element)
+                .closest('.form-group')
+                .addClass('has-error');
+        },
+
+        unhighlight: function(element){
+            $(element)
+                .closest('.form-group')
+                .removeClass('has-error');
+        },
+        errorPlacement: function(error,element){
+            if (element.prop('type') === 'checkbox'){
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
         }
     });
 
